740b5de392937b6c3887d4750f63fb72632da0b2
DerRafDev <rafael.sordi.lino@gmail.com>
2021-05-01 17:23:11 -0400

    Separate Components

---
+++ src/components/Header/NotificationsNav.tsx
@@ -0,0 +1,22 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
+
+
+// this is the Nav for notifications in the Header that will show if a user had
+// add you or if you have another type of notification
+export function NotificationsNav() {
+    return (
+        <HStack
+            spacing="8"
+            mx="8"
+            pr="8"
+            py="1"
+            color="gray.300"
+            borderRightWidth={1}
+            borderColor="gray.700"
+        >
+            <Icon as={RiNotificationLine} fontSize="20" />
+            <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+    );
+}
